a68b68b010db98c2d7d56606fca1647e57631c11
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-28 16:43:03 -0300

    Implementating the sum

--- foo/src/main/java/br/com/felipe/foo/GreetingController.java
+++ foo/src/main/java/br/com/felipe/foo/GreetingController.java
@@ -1,10 +1,11 @@
 package br.com.felipe.foo;
 
-import java.security.PublicKey;
+
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @RequestMapping(value="/sum/numberOne/numberTwo", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne" ) String numberOne , @PathVariable("numberTwo" ) String numberTwo) {
+        return 1D;
     }
 
 }
