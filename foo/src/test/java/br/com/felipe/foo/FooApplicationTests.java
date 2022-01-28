99a93e447c575210a034a79ecd36ed39954fc61e
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-28 16:33:58 -0300

    First commitn

---
+++ foo/src/test/java/br/com/felipe/foo/FooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
