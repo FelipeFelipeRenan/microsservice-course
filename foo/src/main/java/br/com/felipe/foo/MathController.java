362684f1328ea555956c29003f2240de22d06d02
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-28 17:05:32 -0300

    Ending validations

--- foo/src/main/java/br/com/felipe/foo/MathController.java
+++ foo/src/main/java/br/com/felipe/foo/MathController.java
@@ -8,24 +8,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
-   
-    @RequestMapping(value="/sum/numberOne/numberTwo", method = RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne" ) String numberOne , @PathVariable("numberTwo" ) String numberTwo)  throws Exception{
-        if(!isNumeric(numberOne)){
+
+    @RequestMapping(value = "/sum/numberOne/numberTwo", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne)) {
             throw new Exception();
         }
         Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
         return sum;
     }
 
-    private boolean isNumeric(String strNumber){
-        
-        return false;
-    }
-
-    public Double convertToDouble(String strNumber){
+    public Double convertToDouble(String strNumber) {
+        if (strNumber == null) {
+            return 0D;
+        }
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)){
+            return Double.parseDouble(number);
+        }
         return 1D;
 
     }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) {
+            return false;
+        }
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
+
+    }
+
 
 }
