04c2c4f42c84de7aaabc7eb580b51df4aba40e1a
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-07 17:20:05 -0300

    Creating the config server project

---
+++ config-server/src/test/java/com/br/felipe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.felipe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
