e4580be38a024a75cd953febaa5ab94e98dc81a8
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-14 16:26:50 -0300

    Adding env port

--- cambio-service/src/main/java/com/br/felipe/controllers/CambioController.java
+++ cambio-service/src/main/java/com/br/felipe/controllers/CambioController.java
@@ -2,6 +2,8 @@ package com.br.felipe.controllers;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,17 @@ import com.br.felipe.model.Cambio;
 @RequestMapping(value = "cambio-service")
 public class CambioController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") BigDecimal amount,
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		
+		var port = environment.getProperty("local.server.port");
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port );
 	}
 
 }
