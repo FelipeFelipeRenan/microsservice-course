d089dd6f356c319ba881ece142c4b55a426057a8
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-25 16:41:51 -0300

    Customizing routes with Spring Cloud Gateway

--- api-gateway/src/main/java/com/br/felipe/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/br/felipe/configuration/ApiGatewayConfiguration.java
@@ -1,10 +1,6 @@
 package com.br.felipe.configuration;
 
-import java.util.function.Function;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +10,16 @@ public class ApiGatewayConfiguration {
 	
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		Function<PredicateSpec, Buildable<Route>> function = 
-				p -> p.path("/get")
+		return builder.routes()
+				.route(p -> p.path("/get")
 						.filters(f -> f
 								.addRequestHeader("Hello", "World")
 								.addRequestParameter("Hello", "World"))
-						.uri("http://httpbin.org:80");
-		return builder.routes()
-				.route(function )
+						.uri("http://httpbin.org:80") )
+				.route(p -> p.path("/cambio-service/**")
+						.uri("lb://cambio-service"))
+				.route(p -> p.path("/book-service/**")
+						.uri("lb://book-service"))
 				.build();
 	}
 }
