f2060bbd70ad6ad1e245178d453e7152de577516
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-27 17:15:37 -0300

    Adding swagger config class to api gateway

--- api-gateway/src/main/java/com/br/felipe/configuration/OpenAPIConfiguration.java
+++ api-gateway/src/main/java/com/br/felipe/configuration/OpenAPIConfiguration.java
@@ -6,10 +6,15 @@ import java.util.List;
 import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class OpenAPIConfiguration {
+	
+	@Bean
+	@Lazy(value = false)
 	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters configParameters,
 			RouteDefinitionLocator definitionLocator){
 		
@@ -23,10 +28,7 @@ public class OpenAPIConfiguration {
 							GroupedOpenApi.builder().pathsToMatch("/" + name + "/**")
 							.group(name).build();
 							
-						}
-					
-				
-				);
+						});
 		
 		return new ArrayList<>();
 	}
